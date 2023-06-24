8747df55140ec8b9da14adb862023c72ddf88cd2
Aleksandar Buljevic <abuljevic8@gmail.com>
2023-06-24 20:14:58 +0200

    Added TODO

--- README.md
+++ README.md
@@ -7,5 +7,5 @@
 ## Build this image locally
 
 ```shell
-docker build -t aleksandarb99/auth:latest
+docker build . -t aleksandarb99/auth:latest
 ```
--- src/main/java/com/akatsuki/auth/feignclients/AccommodationFeignClient.java
+++ src/main/java/com/akatsuki/auth/feignclients/AccommodationFeignClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(url = "${core.services.accommodation-url}", value = "accommodation-feign-client")
 public interface AccommodationFeignClient {
 
+    //    TODO: Send tokens and fix other microservices than
     @DeleteMapping("/by-host-id/{id}")
     void deleteAccommodationsByHostId(@PathVariable Long id);
 }
